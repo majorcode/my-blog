99fd6493e8e88fd8c0a293d4038552c463d66dfd
Stephen Farmer <sfarmer@majorcode.com>
2022-11-18 15:49:45 -0500

    Revert to v0.4.2 as generated by the docs

--- main.tsx
+++ main.tsx
@@ -1,12 +1,11 @@
-import blog from "https://deno.land/x/blog@0.5.0/blog.tsx";
+/** @jsx h */
+
+import blog, { ga, redirects, h } from "blog";
 
 blog({
   author: "Stephen Farmer",
   title: "majorcode",
   description: 'Why "major?" No reason, really.',
-  // header: <header>Your custom header</header>,
-  // section: <section>Your custom section</section>,
-  // footer: <footer>Your custom footer</footer>,
   avatar: "major.svg",
   avatarClass: "rounded-full",
   links: [
@@ -15,7 +14,7 @@ blog({
     { title: "Twitter", url: "https://twitter.com/majorcode" },
   ],
   lang: "en",
-  dateFormat: (date: Date) => date.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}),
+  // dateFormat: (date: Date) => date.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}),
   canonicalUrl: "https://www.majorcode.com",
   theme: "light",
   favicon: "major.svg",
